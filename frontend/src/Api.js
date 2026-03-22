6daeeb013de96815ec6ddf732101cf2dfabbfe5a
dhirajCodes543 <d4dhirajbarnwal@gmail.com>
2026-03-22 19:21:18 +0530

    frontend deploy

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://65.0.3.226:5000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
