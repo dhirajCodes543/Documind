cc2b7400ad8b304317d257db92a9e731d0ce956e
dhirajCodes543 <d4dhirajbarnwal@gmail.com>
2026-03-27 13:16:31 +0530

    Checking data deleted on new push ?

--- backend/src/middlewares/authmiddleware.js
+++ backend/src/middlewares/authmiddleware.js
@@ -5,7 +5,7 @@ const authMiddleware = (req, res, next) => {
     const token = req.cookies.token;
 
     if (!token) {
-      return res.status(401).json({ error: "Unauthorized: no token provided" });
+      return res.status(401).json({ error: "Unauthorized: No token provided" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
